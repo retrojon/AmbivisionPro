ba6dfc3a97e688bbfb08826076e6c3d66a61c36d
retrojon <46602128+retrojon@users.noreply.github.com>
2025-11-04 15:03:07 +0000

    Update BTPIN.py

--- Bluetooth/BTPIN.py
+++ Bluetooth/BTPIN.py
@@ -1,5 +1,5 @@
 def magiclink_pin(s: str) -> str:
-    # expects s length >= 8 (e.g. "30176509")
+    # expects s length >= 8 (e.g. "87651234")
     A = ord(s[1]); B = ord(s[2]); C = ord(s[3])
     D = ord(s[4]); E = ord(s[5]); F = ord(s[6]); G = ord(s[7])
     part1 = (A * C) ^ (G * B) ^ 0x01
@@ -10,3 +10,4 @@ def magiclink_pin(s: str) -> str:
 # Examples:
 
 print(magiclink_pin("87651234"))  # -> "XXXX"
+
