562abfdf37e732f28541c4f3f72545629c23c195
retrojon <46602128+retrojon@users.noreply.github.com>
2025-11-04 15:01:23 +0000

    Add files via upload
    Magiclink ID Pairing Pin Code

---
+++ Bluetooth/BTPIN.py
@@ -0,0 +1,12 @@
+def magiclink_pin(s: str) -> str:
+    # expects s length >= 8 (e.g. "30176509")
+    A = ord(s[1]); B = ord(s[2]); C = ord(s[3])
+    D = ord(s[4]); E = ord(s[5]); F = ord(s[6]); G = ord(s[7])
+    part1 = (A * C) ^ (G * B) ^ 0x01
+    part2 = (D | (F << 3)) ^ E ^ 0x6D
+    pinnum = (part1 + part2) % 10000
+    return f"{pinnum:04d}"
+
+# Examples:
+
+print(magiclink_pin("87651234"))  # -> "XXXX"
